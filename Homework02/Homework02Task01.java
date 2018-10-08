1cf8e88aacb326136d063079b8772ce47f6b3b70
Arturfahreev <jd-forever@mail.ru>
2018-10-08 07:15:58 +0300

    new02

---
+++ Homework02/Homework02Task01.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+//Пользователь вводит сначала n (кол-во чисел), затем последовательно n чисел. Вывести максимальное по модулю число
+
+public class Homework02Task01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int max = 0;
+        for (int i = 0;i < k; i++) {
+            int x = sc.nextInt();
+            if (max < Math.abs(x)) {
+                max = Math.abs(x);
+            }
+        }
+        System.out.println(max);
+    }
+}
+
