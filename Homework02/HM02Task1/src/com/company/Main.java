80039fd5e58ba1853c3312ba4fcea9d58936ec93
Arturfahreev <jd-forever@mail.ru>
2018-12-26 21:27:14 +0300

    Fixed3

--- Homework02/HM02Task1/src/com/company/Main.java
+++ Homework02/HM02Task1/src/com/company/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        int m;
         int max = 0;
         for (int i = 0;i < k; i++) {
             int x = sc.nextInt();
