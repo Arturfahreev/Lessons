6d4802201f541d1035ac8ecd9be135cbee9de7db
Arturfahreev <jd-forever@mail.ru>
2018-12-26 21:34:07 +0300

    Fixed4

--- Homework02/HW02Task3/src/com/company/Main.java
+++ Homework02/HW02Task3/src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 //Пользователь вводит n, вывести на экран факториал введенного числа
 
-public class Homework02Task03 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
