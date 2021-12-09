ded70210e812d86f06ba275f499602d2a5f9884b
Anton <jd-forever@mail.ru>
2021-12-10 00:29:46 +0300

    Fourth commit

--- ClassTest01/ClassTest03.java
+++ ClassTest01/ClassTest03.java
@@ -2,5 +2,8 @@ public class ClassTest03 {
     public static void main(String[] args) {
         System.out.println("Hello server!");
         System.out.println("Hello!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
