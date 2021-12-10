26175e32d1d29bc1afbbab373050e500713198d7
Anton <jd-forever@mail.ru>
2021-12-10 21:44:38 +0300

    Add average

--- ClassTest01/ClassTest03.java
+++
@@ -1,9 +0,0 @@
-public class ClassTest03 {
-    public static void main(String[] args) {
-        System.out.println("Hello server!");
-        System.out.println("Hello!");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
