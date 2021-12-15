2980ddecd343544346aa2352692b2e44e7c530c4
Anton <jd-forever@mail.ru>
2021-12-15 14:46:56 +0300

    7-th commit

---
+++ ClassTest01/Test08ReverseArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Test08ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n/2; i++){
+            int a = numbers[i];
+            numbers[i] = numbers[n-i-1];
+            numbers[n-i-1] = a;
+        }
+        for (int i = 0; i < n; i++){
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
---
+++ ClassTest01/Test09DeleteChars.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+//Пользователь вводить строку и два числа n1 и n2, затем удаляются из строки эти буквы и выводится новая строка
+public class Test09DeleteChars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int intStart = sc.nextInt();
+        int intFinish = sc.nextInt();
+        String strResult = "";
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (i < intStart || i > intFinish) {
+                strResult += charArray[i];
+            }
+        }
+        System.out.println(strResult);
+
+
+    }
+}
