f215c5c25f9f6142123e723ae76f4c9241a46fae
Anton <jd-forever@mail.ru>
2021-12-27 17:43:42 +0300

    8-th commit

---
+++ ClassTest01/StringToUpperOrLowerCase.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringToUpperOrLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = sc.next();
+
+        System.out.println("Введите код ( l / u ): ");
+        String key = sc.next();
+
+        char[] chars = str.toCharArray();
+        String result = "";
+
+        for (int i = 0; i < chars.length; i++) {
+            if (key.equals("u")) {
+                    result += ( (int) chars[i] >= 97 ) ? ((char) (chars[i] - 32)) : chars[i];
+                } else if (key.equals("l")) {
+                result += ( (int) chars[i] >= 65 && (int) chars[i] <= 90 ) ? (char) (chars[i] + 32) : chars[i];
+            }
+        }
+        System.out.println(result);
+
+    }
+}
