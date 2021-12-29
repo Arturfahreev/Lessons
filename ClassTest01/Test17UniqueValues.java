34225871830ac24591c9159efcb4b19d007d94c4
Anton <jd-forever@mail.ru>
2021-12-29 15:36:02 +0300

    1--th commit

---
+++ ClassTest01/Test17UniqueValues.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test17UniqueValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        boolean result = uniqueValues(numbers);
+        System.out.println(result);
+    }
+    public static boolean uniqueValues (int[] numbersArray) {
+        boolean finalResult = false;
+        for (int i = 0; i < numbersArray.length; i++) {
+            for (int j = i + 1; j < numbersArray.length; j++) {
+                if (numbersArray[i] == numbersArray[j]) finalResult = true;
+            }
+        }
+        return finalResult;
+    }
+}
