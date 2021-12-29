0e5fb5ed2838dc4f5b1086df82f7ce4c5cce900f
Anton <jd-forever@mail.ru>
2021-12-29 19:23:11 +0300

    -a11-th commit

--- ClassTest01/Test17UniqueValues.java
+++ ClassTest01/Test17UniqueValues.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//Вводится массив чисел и определяется есть ли одинаковые элементы в нем
 
 public class Test17UniqueValues {
     public static void main(String[] args) {
@@ -15,7 +16,10 @@ public class Test17UniqueValues {
         boolean finalResult = false;
         for (int i = 0; i < numbersArray.length; i++) {
             for (int j = i + 1; j < numbersArray.length; j++) {
-                if (numbersArray[i] == numbersArray[j]) finalResult = true;
+                if (numbersArray[i] == numbersArray[j]) {
+                    finalResult = true;
+                    break;
+                }
             }
         }
         return finalResult;
